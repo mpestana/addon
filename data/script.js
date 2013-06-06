c820aa5d1d1b7d8625643bdfb2f3d971b0e36eeb
camila <ca.mila.souzaaraujo@gmail.com>
2013-06-06 14:31:28 -0300

    facebook api

--- data/script.js
+++ data/script.js
@@ -1,6 +1,41 @@
 self.port.on("init", function init(flag) {
 	if (window.frameElement) return;
 
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '253368234806475', // App ID
+			channelUrl : 'channel.html', // Channel File
+			status     : true, // check login status
+			cookie     : true, // enable cookies to allow the server to access the session
+			xfbml      : true  // parse XFBML
+		});
+
+		FB.Event.subscribe('auth.authResponseChange', function(response) {
+			if (response.status === 'connected') {
+				testAPI();
+			} else if (response.status === 'not_authorized') {
+				FB.login();
+			} else {
+				FB.login();
+			}
+		});
+	};
+
+	(function(d){
+		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+		if (d.getElementById(id)) {return;}
+			js = d.createElement('script'); js.id = id; js.async = true;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			ref.parentNode.insertBefore(js, ref);
+	}(document));
+
+	function testAPI() {
+		console.log('Welcome!  Fetching your information.... ');
+		FB.api('/me', function(response) {
+			console.log('Good to see you, ' + response.name + '.');
+		});
+	}
+	
 	var first_div = document.createElement("div");
 	first_div.setAttribute("id", "first_div");
 	first_div.className = "first_div";
@@ -85,6 +120,12 @@ self.port.on("init", function init(flag) {
 		first_div.appendChild(second_div);
 		document.body.appendChild(first_div); 
 	}
+	
+	var FaceBook = document.createElement('div');
+	FaceBook.innerHTML = '<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>';
+	document.body.appendChild(FaceBook);
+	
+	
 });
 
 
